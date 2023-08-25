ff9df19d6870b9715d45d076f4ebb72a0dcdc175
Shamainb <shamainbhengesa60@gmail.com>
2023-08-25 08:42:40 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -8,51 +8,32 @@
  */
 int _printf(const char *format, ...)
 {
+	char *s;
+	char c;
 	int i;
 	int count = 0;
 	va_list args;
 
-	va_start (arg, format);
+	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-#include <stdio.h>
-#include "main.h"
-
-/**
- * _printf - function that produces output
- * @format:  is a character string
- * Return: characters printed if successful
- */
-int _printf(const char *format, ...)
 {
-	char *s;
-	char c;
-        int i;
-        int count = 0;
-        va_list args;
-
-        va_start (args, format);
-
-        for (i = 0; format[i] != '\0'; i++)
-        {
-                if (format[i] == '%')
-                {
-                        i++;
-                if (format[i] == 'c')
-			c = va_arg(args, int);
-		_putchar(c);
-		count++;
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+				c = va_arg(args, int);
+			putchar(c);
+			count++;
 		}
 		else if (format[i] == 's')
 		{
-			*s = va_arg(args, char *);
+			s = va_arg(args, char *);
 			if (s != NULL)
 			{
 				while (*s != '\0')
 				{
-					_putchar(*s);
+					putchar(*s);
 					s++;
 					count++;
 				}
@@ -60,15 +41,15 @@ int _printf(const char *format, ...)
 		}
 		else if (format[i]  == '%')
 		{
-			_putchar('%');
-			count++;
-                }
-		else
-		{
-			_putchar(format[i]);
+			putchar('%');
 			count++;
 		}
+		else
+	{
+			putchar(format[i]);
+			count++;
 	}
+}
 va_end(args);
 return (count);
 }
